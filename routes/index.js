2128f89a8e57d18085117b55b0ec7f28984c8465
santhosh2521 <santhosh600sk@gmail.com>
2025-04-05 00:16:07 +0530

    let's hope

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express    = require('express');
 const router     = express.Router();
 var cookieParser = require('cookie-parser');
 const { encode } = require('punycode');
-
+const sanitize = require('sanitize-html')
 var idd=0;
 const response = data => ({ message: data });
 /* GET home page. */
@@ -19,21 +19,26 @@ router.post('/',function(req,res,next) //when users login
       {
         res.status(500).send(response("Something's wrong!! Try again"));
       }
-      else if(req.body.username==='admin' && req.body.password!=='$up3R_$3cR3T$$')
+      else if(req.body.username==='admin' && req.body.password!=='$up3R_$3cR3T$$123')
       {
         res.status(500).send(response("Something's wrong!! Try again"));
       }
       else
       {
-        var jstring = JSON.stringify({user:req.body.username,id:++idd})
+
+        var jstring = JSON.stringify({user:sanitize(req.body.username),id:++idd})
         var encoded = btoa(jstring);
         res.cookie('user',encoded);
-        res.json({"message":"ok"});
+        res.json({"message":"Logging in!!"});
       }
 });
 
 router.get('/user',function(req,res,next) //the page for normal users
 {
+    if(req.cookies.user==="")
+    {
+      res.send(response("Bruhh!!! Login again"))
+    }
     var jstring = atob(req.cookies.user);
     var parsed = JSON.parse(jstring);
     if(parsed.user ==="admin" && parsed.id===0)
